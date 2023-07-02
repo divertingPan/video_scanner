80423e2dfd6ca59b73adbcab300074c643b39a67
Diverting Pan <39986072+divertingPan@users.noreply.github.com>
2023-07-02 13:02:11 +0800

    Delete load_window.py

--- load_window.py
+++
@@ -1,9 +0,0 @@
-from contextlib import suppress
-with suppress(ModuleNotFoundError):
-    import pyi_splash
-
-    pyi_splash.update_text('loaded...')
-    pyi_splash.close()
-
-
-# pyinstaller command: pyinstaller -F -w --splash=<path-to-image> --icon=<path-to-icon> <your-script>.py
